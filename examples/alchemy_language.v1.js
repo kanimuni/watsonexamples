bdfb90ae1fa13767d586c98a9991f7748e9f0778
kanimuni <kani_munidasa@vielabs.com>
2016-06-17 15:35:09 -0700

    push all

---
+++ examples/alchemy_language.v1.js
@@ -0,0 +1,18 @@
+'use strict';
+var config = require('../env/config.js');
+var watson = require('watson-developer-cloud');
+
+var alchemy_language = watson.alchemy_language({
+  api_key: config.api_key
+});
+
+var params = {
+  text: 'IBM Watson won the Jeopardy television show hosted by Alex Trebek'
+};
+
+alchemy_language.sentiment(params, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
